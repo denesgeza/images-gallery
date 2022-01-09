a7cc10f763d89b24f423cdba643a92c6272295a0
Geza Denes <denesgeza@gmail.com>
2022-01-09 11:42:12 +0200

    welcome component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,11 +6,11 @@ const navbarStyle = {
   backgroundColor: '#eeeeee',
 };
 
-const Header = (props) => {
+const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Logo style={{ maxWidth: '14rem', maxHeight: '2rem' }} />
+        <Logo alt={title} style={{ maxWidth: '14rem', maxHeight: '2rem' }} />
       </Container>
     </Navbar>
   );
