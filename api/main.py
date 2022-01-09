bbefadf7f61e12b4fbc769da37adaf35b25baa73
Geza Denes <denesgeza@gmail.com>
2022-01-09 20:22:33 +0200

    enabled pylint and black code formatter

--- api/main.py
+++ api/main.py
@@ -1,33 +1,37 @@
+import os
 from flask import Flask, request
 import requests
-import os
 from dotenv import load_dotenv
 from flask_cors import CORS
 
 
-load_dotenv(dotenv_path='./.env.local')
+load_dotenv(dotenv_path="./.env.local")
 
-UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
-UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", '')
-DEBUG = bool(os.environ.get('DEBUG', True))
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
-    raise EnvironmentError('Please create .env.local file and insert there `UNSPLASH_KEY`')
+    raise EnvironmentError(
+        "Please create .env.local file and insert there `UNSPLASH_KEY`"
+    )
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['DEBUG'] = DEBUG
+app.config["DEBUG"] = DEBUG
+
 
 @app.route("/new-image")
 def new_image():
-    word = request.args.get('query')
+    ''' Returns an image based on the search parameters'''
+    word = request.args.get("query")
 
     headers = {
-        'Accept-Version': 'v1',
-        'Authorization': 'Client-ID ' + UNSPLASH_KEY,
+        "Accept-Version": "v1",
+        "Authorization": "Client-ID " + UNSPLASH_KEY,
     }
-    params = {'query': word}
+    params = {"query": word}
 
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
@@ -35,4 +39,4 @@ def new_image():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0' , port=5050)
+    app.run(host="0.0.0.0", port=5050)
